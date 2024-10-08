9f9bcb25f047f7f924d7d6e11ef8db5ac32f1530
aswin186 <naswin186@gmail.com>
2024-10-08 22:46:53 +0530

    added allowed hosts and csrf tokens for deployment

--- shopcart/settings.py
+++ shopcart/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'shopingkart.pythonanywhere.com']
+CSRF_TRUSTED_ORIGINS = ['https://shopingkart.pythonanywhere.com/']
 
 
 # Application definition
