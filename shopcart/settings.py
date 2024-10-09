2e54734a3eebc35ce166d8d8ff841d6478e79854
aswin186 <naswin186@gmail.com>
2024-10-09 11:40:13 +0530

    final configuration for the site luanch with media and media root settings changed

--- shopcart/settings.py
+++ shopcart/settings.py
@@ -156,9 +156,12 @@ STATICFILES_DIRS = [
 ]
 
 # Media files (Images, Files)
+#
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
